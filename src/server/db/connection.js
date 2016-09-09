ce7b0487368364c8e933c2e833085b8af6803a1f
alnye655321 <alnye655321@gmail.com>
2016-09-09 04:10:31 -0600

    connection mod

--- src/server/db/connection.js
+++ src/server/db/connection.js
@@ -5,7 +5,9 @@ const pgp = require('pg-promise')(options);
 const cn = {
   host: 'localhost', // 'localhost' is the default;
   port: 5432, // 5432 is the default;
-  database: 'galvanize_reads'
+  database: 'galvanize_reads',
+  username: 'test1',
+  password: 'Pqwoalstest1?'
 };
 
 const db = pgp(cn); // database instance;
