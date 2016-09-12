651ac8058c4d4e7b838014c34e2afc3e89244404
Alex Nye <alnye655321@gmail.com>
2016-09-11 19:26:27 -0600

    finished update and delete

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -57,7 +57,7 @@ $(document).on('submit', '#modal-book-form', function(event) {
   };
   $.ajax({
     type: 'PUT',
-    url: `/api/v1/${$bookID}`,
+    url: `/api/v1/books/${$bookIDUpdate}`,
     data: payload
   })
   .done((data) => {
@@ -68,4 +68,89 @@ $(document).on('submit', '#modal-book-form', function(event) {
   .fail((err) => {
     console.log(err);
   });
+});
+
+//fill author update modal form
+$(document).on('click', '.update-author-btn', function() {
+  const $this = $(this);
+  const authorID = $this.attr('data-id');
+  const first_nameUpdate = $this.attr('data-first_name');
+  const last_nameUpdate = $this.attr('data-last_name');
+  const biographyUpdate = $this.attr('data-biography');
+  const portraitUpdate = $this.attr('data-portrait');
+  $('#form-first_name').val(first_nameUpdate);
+  $('#form-last_name').val(last_nameUpdate);
+  $('#form-biography').val(biographyUpdate);
+  $('#form-portrait').val(portraitUpdate);
+  $('#form-id').val(authorID);
+});
+
+//submit author update modal form
+$(document).on('submit', '#modal-author-form', function(event) {
+  event.preventDefault();
+  const $first_nameUpdate = $('#form-first_name').val();
+  const $last_nameUpdate = $('#form-last_name').val();
+  const $biographyUpdate = $('#form-biography').val();
+  const $portraitUpdate = $('#form-portrait').val();
+  const $authorIDUpdate = $('#form-id').val();
+  const payload = {
+    first_name: $first_nameUpdate,
+    last_name: $last_nameUpdate,
+    biography: $biographyUpdate,
+    portrait: $portraitUpdate,
+    id: $authorIDUpdate
+  };
+  $.ajax({
+    type: 'PUT',
+    url: `/api/v1/authors/${$authorIDUpdate}`,
+    data: payload
+  })
+  .done((data) => {
+    $('#myModal').modal('toggle');
+    location.reload();
+    console.log(data);
+  })
+  .fail((err) => {
+    console.log(err);
+  });
+});
+
+// delete author button
+$(document).on('click', '.delete-author-btn', function() {
+  const answer = confirm('Are you sure?');
+  if (answer) {
+    const $this = $(this);
+    const authorID = $this.attr('data-id');
+    $.ajax({
+      type: 'DELETE',
+      url: `/api/v1/authors/${authorID}`
+    })
+    .done((data) => {
+      location.reload();
+      console.log(data);
+    })
+    .fail((err) => {
+      console.log(err);
+    });
+  }
+});
+
+// delete book button
+$(document).on('click', '.delete-book-btn', function() {
+  const answer = confirm('Are you sure?');
+  if (answer) {
+    const $this = $(this);
+    const bookID = $this.attr('data-id');
+    $.ajax({
+      type: 'DELETE',
+      url: `/api/v1/books/${bookID}`
+    })
+    .done((data) => {
+      location.reload();
+      console.log(data);
+    })
+    .fail((err) => {
+      console.log(err);
+    });
+  }
 });
