589293b1144ee42251e519c2f34850a741324b97
alnye655321 <alnye655321@gmail.com>
2016-09-11 18:29:01 -0600

    added books update modal form

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -24,3 +24,48 @@ $(document).ready(function(){
 $( "#filter" ).on( "click", function() {
   $( "#filterForm" ).toggle();
 });
+
+//fill book update modal form
+$(document).on('click', '.update-book-btn', function() {
+  const $this = $(this);
+  const bookID = $this.attr('data-id');
+  const description = $this.attr('data-description');
+  const cover = $this.attr('data-cover');
+  const genre = $this.attr('data-genre');
+  const title = $this.attr('data-title');
+  $('#form-title').val(title);
+  $('#form-genre').val(genre);
+  $('#form-cover').val(cover);
+  $('#form-description').val(description);
+  $('#form-id').val(bookID);
+});
+
+//submit book update modal form
+$(document).on('submit', '#modal-book-form', function(event) {
+  event.preventDefault();
+  const $descriptionUpdate = $('#form-description').val();
+  const $coverUpdate = $('#form-cover').val();
+  const $genreUpdate = $('#form-genre').val();
+  const $titleUpdate = $('#form-title').val();
+  const $bookIDUpdate = $('#form-id').val();
+  const payload = {
+    description: $descriptionUpdate,
+    cover: $coverUpdate,
+    genre: $genreUpdate,
+    title: $titleUpdate,
+    id: $bookIDUpdate
+  };
+  $.ajax({
+    type: 'PUT',
+    url: `/api/v1/${$bookID}`,
+    data: payload
+  })
+  .done((data) => {
+    $('#myModal').modal('toggle');
+    location.reload();
+    console.log(data);
+  })
+  .fail((err) => {
+    console.log(err);
+  });
+});
