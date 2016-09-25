c8fa0ab9d41d044cd978bdf5dfcfcbf256afe8dd
Alex <alnye655321@gmail.com>
2016-09-24 20:37:22 -0600

    added cors

--- src/server/routes/authors.js
+++ src/server/routes/authors.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const db = require('../db/connection');
 
 router.get('/', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //res.header('Access-Control-Allow-Headers', 'Content-Type');
   const renderObject = {};
   renderObject.title = 'Authors';
   db.any('SELECT * FROM authors')
